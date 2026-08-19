8772d7c34496c45c258bb255e1191935574ee03b
conache <C.conache@gmail.com>
2026-08-19 14:18:42 +0300

    Scaffold gossip verification of inclusion lists

---
+++ beacon_node/beacon_chain/src/inclusion_list_verification/gossip_verified_inclusion_list.rs
@@ -0,0 +1,68 @@
+use crate::inclusion_list_verification::InclusionListVerificationError;
+use crate::{BeaconChain, BeaconChainTypes};
+use std::sync::Arc;
+use tracing::debug;
+use types::{ChainSpec, SignedInclusionList};
+
+pub struct GossipVerifiedInclusionListContext<'a, T: BeaconChainTypes> {
+    // TODO(heze): complete while implementing the gossip verification of inclusion lists
+    pub slot_clock: &'a T::SlotClock,
+    pub spec: &'a ChainSpec,
+}
+
+pub struct GossipVerifiedInclusionList {
+    pub signed_inclusion_list: Arc<SignedInclusionList>,
+}
+
+impl GossipVerifiedInclusionList {
+    pub fn new<T: BeaconChainTypes>(
+        signed_inclusion_list: Arc<SignedInclusionList>,
+        _ctx: &GossipVerifiedInclusionListContext<'_, T>,
+    ) -> Result<Self, InclusionListVerificationError> {
+        // TODO(heze): implement gossip verification for inclusion lists
+        Ok(Self {
+            signed_inclusion_list,
+        })
+    }
+}
+
+impl<T: BeaconChainTypes> BeaconChain<T> {
+    pub fn inclusion_list_verification_context(&self) -> GossipVerifiedInclusionListContext<'_, T> {
+        GossipVerifiedInclusionListContext {
+            slot_clock: &self.slot_clock,
+            spec: &self.spec,
+        }
+    }
+
+    pub fn verify_inclusion_list_for_gossip(
+        &self,
+        signed_inclusion_list: Arc<SignedInclusionList>,
+    ) -> Result<GossipVerifiedInclusionList, InclusionListVerificationError> {
+        let slot = signed_inclusion_list.message.slot;
+        let validator_index = signed_inclusion_list.message.validator_index;
+
+        let ctx = self.inclusion_list_verification_context();
+        match GossipVerifiedInclusionList::new(signed_inclusion_list, &ctx) {
+            Ok(verified) => {
+                debug!(
+                    %slot,
+                    %validator_index,
+                    "Successfully verified gossip inclusion list"
+                );
+
+                // TODO(heze): emit the inclusion_list SSE event
+
+                Ok(verified)
+            }
+            Err(e) => {
+                debug!(
+                    error = e.to_string(),
+                    %slot,
+                    %validator_index,
+                    "Rejected gossip inclusion list"
+                );
+                Err(e)
+            }
+        }
+    }
+}
---
+++ beacon_node/beacon_chain/src/inclusion_list_verification/mod.rs
@@ -0,0 +1,35 @@
+use crate::BeaconChainError;
+use std::sync::Arc;
+use types::{BeaconStateError, Slot};
+
+pub mod gossip_verified_inclusion_list;
+
+#[derive(Debug)]
+pub enum InclusionListVerificationError {
+    // Two valid inclusion lists were already seen from this validator for this slot.
+    AlreadySeenTwice { validator_index: u64, slot: Slot },
+    // The slot clock cannot read.
+    UnableToReadSlot,
+    // Beacon Chain error
+    BeaconChainError(Arc<BeaconChainError>),
+    // Beacon State error
+    BeaconStateError(BeaconStateError),
+}
+
+impl std::fmt::Display for InclusionListVerificationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl From<BeaconChainError> for InclusionListVerificationError {
+    fn from(e: BeaconChainError) -> Self {
+        InclusionListVerificationError::BeaconChainError(Arc::new(e))
+    }
+}
+
+impl From<BeaconStateError> for InclusionListVerificationError {
+    fn from(e: BeaconStateError) -> Self {
+        InclusionListVerificationError::BeaconStateError(e)
+    }
+}
